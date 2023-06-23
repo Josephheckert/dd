b7f5677d6b9e81076ca8053d153a2b864c13dede
Josephheckert <69606492+Josephheckert@users.noreply.github.com>
2023-06-23 07:30:43 -0400

    Update races.py

--- races.py
+++ races.py
@@ -1,147 +1,360 @@
-class Race:
-
-    def __init__(self, race_name, race_subcategory, scores_increase, age, size, speed, languages, abilities, proficiencies):
-        self.race_name = race_name
-        self.race_subcategory = race_subcategory
-        self.scores_increase = scores_increase
-        self.age = age
-        self.size = size
-        self.speed = speed
-        self.languages = languages
-        self.abilities = abilities
-        self.proficiencies = proficiencies
-
-dragonborn = [
-    {"Race Name" : "Dragonborn"},
-    {"Race Subcategory" : ["Black", "Blue", "Brass", "Bronze", "Copper", "Gold", "Green", "Red", "Silver", "White"]},
-    {"Scores Increase" : [{"strength" : 1}, {"charisma" : 1}]},
-    {"Age" : 15},
-    {"Size" : "Medium"},
-    {"Speed" : 30},
-    {"Languages" : ["Common", "Draconic"]},
-    {"Abilities" :  
+race_data = [
+    [
+        {"Race Name" : "Dragonborn"},
+        {"Race Subcategory" : ["Black", "Blue", "Brass", "Bronze", "Copper", "Gold", "Green", "Red", "Silver", "White"]},
+        {"Name Suggestions" : [
+                {"Male" :
+                    ["Arjhan", "Balasar", "Bharash", "Donaar", "Ghesh", "Heskan", "Kriv", "Medrash", "Mehen", "Nadarr", "Pandjed", "Patrin", "Rhogar", "Shamash", "Shedinn", "Tarhun", "Torinn"]
+                },
+                {"Female" :
+                    ["Akra", "Biri", "Daar", "Farideh", "Harann", "Havilar", "Jheri", "Kava", "Korinn", "Mishann", "Nala", "Perra", "Raiann", "Sora", "Surina", "Thava", "Uadjit"]
+                },
+                {"Childhood" :
+                    ["Climber", "Earbender", "Leaper", "Pious", "Shieldbiter", "Zealous"]
+                },
+                {"Clan" :
+                    ["Clethtinthiallor", "Daardendrian", "Delmirev", "Drachedandion", "Fenkenkabradon", "Kepeshkmolik", "Kerrhylon", "Kimbatuul", "Linxakasendalor", "Myastan", "Nemmonis", "Norixius", "Ophinshtalajiir", "Prexijandilin", "Shestendeliath", "Turnuroth", "Verthisathurgiesh", "Yarjerit"]
+                },
+            ]
+        },
+        {"Scores Increase" : [{"strength" : 1}, {"charisma" : 1}]},
+        {"Age" : 15},
+        {"Size" : "Medium"},
+        {"Speed" : 30},
+        {"Languages" : ["Common", "Draconic"]},
+        {"Abilities" :  
+            [
+                {"Damage Resistance" : 
                     [
-                        {"Damage Resistance" : 
-                            [
-                                {"Black" : "Acid"},
-                                {"Blue" : "Lightning"},
-                                {"Brass" : "Fire"},
-                                {"Bronze" : "Lightning"},
-                                {"Copper" : "Acid"},
-                                {"Gold" : "Fire"},
-                                {"Green" : "Poison"},
-                                {"Red" : "Fire"},
-                                {"Silver" : "Cold"},
-                                {"White" : "Cold"}
-                            ]
-                        },
-
-                        {"Breath Weapon" :
-                            [
-                                {"Black" : "5 by 30 ft. line (Dex. save)"},
-                                {"Blue" : "5 by 30 ft. line (Dex. save)"},
-                                {"Brass" : "5 by 30 ft. line (Dex. save)"},
-                                {"Bronze" : "5 by 30 ft. line (Dex. save)"},
-                                {"Copper" : "5 by 30 ft. line (Dex. save)"},
-                                {"Gold" : "15 ft. cone (Dex. save)"},
-                                {"Green" : "15 ft. cone (Con. save)"},
-                                {"Red" : "15 ft. cone (Dex. save)"},
-                                {"Silver" : "15 ft. cone (Con. save)"},
-                                {"White" : "15 ft. cone (Con. save)"}
-                            ]
-                        }
+                        {"Black" : "Acid"},
+                        {"Blue" : "Lightning"},
+                        {"Brass" : "Fire"},
+                        {"Bronze" : "Lightning"},
+                        {"Copper" : "Acid"},
+                        {"Gold" : "Fire"},
+                        {"Green" : "Poison"},
+                        {"Red" : "Fire"},
+                        {"Silver" : "Cold"},
+                        {"White" : "Cold"}
                     ]
-    }
-]
+                },
 
-dwarf = [
-    {"Race Name" : "Dwarf"},
-    {"Race Subcategory" : ["Hill Dwarf", "Mountain Dwarf"]},
-    {"Scores Increase" : 
-        [
-            {"Hill Dwarf" :
-                [
-                    {"constitution" : 2}, 
-                    {"charisma" : 1},
-                    {"strength" : 1}
-                ]
-            },
-            {"Mountain Dwarf" :
-                [
-                    {"constitution" : 2}, 
-                    {"charisma" : 1},
-                    {"wisdom" : 1},
-                    {"toughness" : 1}
-                ] 
-            },
-        ]
-    },
-    {"Age" : 15},
-    {"Size" : "Medium"},
-    {"Speed" : 30},
-    {"Languages" : 
-        ["Common", "Dwarvish"]},
-    {"Abilities" :  
-        [
-            "Darkvision",
-            "Dwarven Resilience"
-            "Stonecunning"
-        ]
-    },
-    {"Proficiencies" : 
-        [
-            {"Weapons" : ["battleaxe", "handaxe", "light hammer", "war hammer"]},
-            {"Tools" : ["smith", "brewer", "mason"]}
-        ]
+                {"Breath Weapon" :
+                    [
+                        {"Black" : "5 by 30 ft. line (Dex. save)"},
+                        {"Blue" : "5 by 30 ft. line (Dex. save)"},
+                        {"Brass" : "5 by 30 ft. line (Dex. save)"},
+                        {"Bronze" : "5 by 30 ft. line (Dex. save)"},
+                        {"Copper" : "5 by 30 ft. line (Dex. save)"},
+                        {"Gold" : "15 ft. cone (Dex. save)"},
+                        {"Green" : "15 ft. cone (Con. save)"},
+                        {"Red" : "15 ft. cone (Dex. save)"},
+                        {"Silver" : "15 ft. cone (Con. save)"},
+                        {"White" : "15 ft. cone (Con. save)"}
+                    ]
+                }
+            ]
+        },
+        {"Proficiencies" : 
+            [
 
-    }
-]
+            ]
+        }
+    ],
 
-elf = [
-    {"Race Name" : "Elf"},
-    {"Race Subcategory" : ["High Elf", "Wood Elf"]},
-    {"Scores Increase" : 
-        [
-            {"High Elf" :
-                [
-                    {"dexterity" : 1}, 
-                    {"intelligence" : 1},
-                ]
-            },
-            {"Wood Elf" :
-                [
-                    {"dexterity" : 1}, 
-                    {"wisdom" : 1},
-                ] 
-            },
-        ]
-    },
-    {"Age" : 15},
-    {"Size" : "Medium"},
-    {"Speed" : 
-        [
-            {"High Elf" : 30},
-            {"Wood Elf" : 35}
-        ] 
-    },
-    {"Languages" : 
-        [
-            {"High Elf" : ["Common", "Elvish", "Undecided"]},
-            {"Wood Elf" : ["Common", "Elvish"]}
-        ] 
-    },
-    {"Abilities" :  
-        [
-            "Darkvision",
-            "Fey Ancestry"
-            "Trance"
-        ]
-    },
-    {"Proficiencies" : 
-        [
-            {"Weapons" : ["longsword", "shortsword", "shortbow", "longbow"]},
-            {"Skills" : ["perception"]}
-        ]
+    [
+        {"Race Name" : "Dwarf"},
+        {"Race Subcategory" : ["Hill Dwarf", "Mountain Dwarf"]},
+        {"Scores Increase" : 
+            [
+                {"Hill Dwarf" :
+                    [
+                        {"constitution" : 2}, 
+                        {"charisma" : 1},
+                        {"strength" : 1}
+                    ]
+                },
+                {"Mountain Dwarf" :
+                    [
+                        {"constitution" : 2}, 
+                        {"charisma" : 1},
+                        {"wisdom" : 1},
+                        {"toughness" : 1}
+                    ] 
+                },
+            ]
+        },
+        {"Age" : 15},
+        {"Size" : "Medium"},
+        {"Speed" : 30},
+        {"Languages" : 
+            ["Common", "Dwarvish"]},
+        {"Abilities" :  
+            [
+                "Darkvision",
+                "Dwarven Resilience"
+                "Stonecunning"
+            ]
+        },
+        {"Proficiencies" : 
+            [
+                {"Weapons" : ["battleaxe", "handaxe", "light hammer", "war hammer"]},
+                {"Tools" : ["smith", "brewer", "mason"]}
+            ]
 
-    }
+        }
+    ],
+
+    [
+        {"Race Name" : "Elf"},
+        {"Race Subcategory" : ["High Elf", "Wood Elf"]},
+        {"Scores Increase" : 
+            [
+                {"High Elf" :
+                    [
+                        {"dexterity" : 1}, 
+                        {"intelligence" : 1},
+                    ]
+                },
+                {"Wood Elf" :
+                    [
+                        {"dexterity" : 1}, 
+                        {"wisdom" : 1},
+                    ] 
+                },
+            ]
+        },
+        {"Age" : 15},
+        {"Size" : "Medium"},
+        {"Speed" : 
+            [
+                {"High Elf" : 30},
+                {"Wood Elf" : 35}
+            ] 
+        },
+        {"Languages" : 
+            [
+                {"High Elf" : ["Common", "Elvish"]},
+                {"Wood Elf" : ["Common", "Elvish"]}
+            ] 
+        },
+        {"Abilities" :  
+            [
+                "Darkvision",
+                "Fey Ancestry",
+                "Trance"
+            ]
+        },
+        {"Proficiencies" : 
+            [
+                {"Weapons" : ["longsword", "shortsword", "shortbow", "longbow"]},
+                {"Skills" : ["perception"]}
+            ]
+
+        }
+    ],
+
+    [
+        {"Race Name" : "Gnome"},
+        {"Race Subcategory" : ["Deep Gnome", "Rock Gnome"]},
+        {"Scores Increase" : 
+            [
+                {"Deep Gnome" :
+                    [
+                        {"intelligence" : 2}, 
+                        {"dexterity" : 1}
+                    ]
+                },
+                {"Mountain Dwarf" :
+                    [
+                        {"intelligence" : 2}, 
+                        {"constitution" : 1}
+                    ] 
+                },
+            ]
+        },
+        {"Age" : 25},
+        {"Size" : "Small"},
+        {"Speed" : 25},
+        {"Languages" : 
+            [
+                {"Deep Gnome" :
+                    ["Common", "Gnomish"]
+                },
+                {"Deep Gnome" :
+                    ["Common", "Gnomish", "Uncommon"]
+                }
+            ]
+        },
+        {"Abilities" :  
+            [
+                {"Deep Gnome" :
+                    [
+                        "Superior Darkvision", 
+                        "Gnome Cunning",
+                        "Stone Camoflage"
+                    ]
+                },
+                {"Rock Gnome" :
+                    [
+                        "Darkvision", 
+                        "Gnome Cunning",
+                        "Artificer's Lore",
+                        "Tinker"
+                    ]
+                }
+            ]
+        },
+        {"Proficiencies" : 
+            [
+
+            ]
+        }
+    ],
+
+    [
+        {"Race Name" : "Half-Elf"},
+        {"Race Subcategory" : []},
+        {"Scores Increase" : 
+            [{"charisma" : 2}]
+        },
+        {"Age" : 20},
+        {"Size" : "Medium"},
+        {"Speed" : 30},
+        {"Languages" : 
+            ["Common", "Elvish"]},
+        {"Abilities" :  
+            [
+                "Darkvision",
+                "Fey Ancestry"
+            ]
+        },
+        {"Proficiencies" : 
+            [
+
+            ]
+        }
+    ],
+
+    [
+        {"Race Name" : "Halfling"},
+        {"Race Subcategory" : ["Lihgtfoot Halfling", "Stout Halfling"]},
+        {"Scores Increase" : 
+            [
+                {"Lightfoot Halfling" :
+                    [
+                        {"dexterity" : 2}, 
+                        {"charisma" : 1}
+                    ]
+                },
+                {"Stout Halfling" :
+                    [
+                        {"dexterity" : 2},
+                        {"constitution" : 1}
+                    ] 
+                },
+            ]
+        },
+        {"Age" : 20},
+        {"Size" : "Small"},
+        {"Speed" : 25},
+        {"Languages" : 
+            ["common", "halfling"]
+        },
+        {"Abilities" :  
+            [
+                {"Lihgtfoot Halfling" :
+                    [
+                        "Lucky", 
+                        "Brave",
+                        "Halfling Nimbleness",
+                        "Naturally Stealthy"
+                    ]
+                },
+                {"Rock Gnome" :
+                    [
+                        "Lucky", 
+                        "Brave",
+                        "Halfling Nimbleness",
+                        "Stout Resilience"
+                    ]
+                }
+            ]
+        },
+        {"Proficiencies" : 
+            [
+
+            ]
+        }
+    ],
+
+    [
+        {"Race Name" : "Half-Orc"},
+        {"Race Subcategory" : []},
+        {"Scores Increase" : 
+            [{"strength" : 2}, {"constitution" : 1}]
+        },
+        {"Age" : 14},
+        {"Size" : "Medium"},
+        {"Speed" : 30},
+        {"Languages" : ["Common", "Orc"]},
+        {"Abilities" :  
+            [
+                "Darkvision",
+                "Relentless Endurance",
+                "Savage Attacks"
+            ]
+        },
+        {"Proficiencies" : 
+            [
+                {"skills" : "intimidation"}
+            ]
+        }
+    ],
+
+    [
+        {"Race Name" : "Human"},
+        {"Race Subcategory" : []},
+        {"Scores Increase" : 
+            []
+        },
+        {"Age" : 18},
+        {"Size" : "Medium"},
+        {"Speed" : 30},
+        {"Languages" : ["Common"]},
+        {"Abilities" :  
+            [
+
+            ]
+        },
+        {"Proficiencies" : 
+            [
+
+            ]
+        }
+    ],
+
+    [
+        {"Race Name" : "Tiefling"},
+        {"Race Subcategory" : []},
+        {"Scores Increase" : 
+            [{"charisma" : 2}, {"intelligence" : 1}]
+        },
+        {"Age" : 18},
+        {"Size" : "Medium"},
+        {"Speed" : 30},
+        {"Languages" : ["Common", "Infernal"]},
+        {"Abilities" :  
+            [
+                "Darkvision",
+                "Hellish Resistance",
+                "Infernal Legacy"
+            ]
+        },
+        {"Proficiencies" : 
+            [
+
+            ]
+        }
+    ]
 ]
